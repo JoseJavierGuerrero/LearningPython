dd8ff6f6399bfcc0b6724f0ef02fa1428a38d42b
Jose J Guerrero <jjguerreromontero@gmail.com>
2011-04-04 23:19:45 +0200

    Tipos basicos en Python

---
+++ TiposBasicos.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+
+# Los tipos basicos de Python son:
+
+#	Numeros
+entero = 23
+print entero
+print type(entero)
+
+#	Cadenas
+cadena = "Hola mundo"
+print cadena
+print type(cadena)
+
+#	Booleanos
+verdad = True
+print verdad
+print type(verdad)
+
+# Como podemos observar, no es necesario especificar el tipo 
+# de la variable, Python lo infiere de su contenido   
+
+# Los operadores aritméticos, a nivel de bit y booleanos son los usuales
+# En cadenas se concatena con el operador +, y cadena * n permite replicar
+# cadena n veces
+
+
