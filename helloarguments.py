59d793a00a33a373da2576d0d7f39b1f9a3e999c
alejandrogomez <alejandroogomez@gmail.com>
2011-05-23 13:42:52 +0200

    arguments example

---
+++ helloarguments.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def person(name='Edgar', age=18):
+	print name + ' is ' + str(age)
+
+
+if __name__ == '__main__':
+	person()
+	person('Miles')
+	person(age=21)
+	person('Damian', 23)
+	person(age=30, name='Herman')
+	
+
