8b2f66f4bfb1b98a77db1a96494faa96c837af08
alejandrogomez <alejandroogomez@gmail.com>
2011-05-24 23:16:28 +0200

    filterlists modified

--- hellolambda.py
+++ hellolambda.py
@@ -2,9 +2,7 @@
 # encoding: utf-8
 
 def main():
-	"""
-	Definition of map function using list comprehension.
-	"""
+	"""Definition of map function using list comprehension."""
 	map = lambda f, list: [f(elem) for elem in list]
 	numberList = [1, 2, 3, 4, 5,]
 	print map (lambda x: pow(x, 2), numberList)
