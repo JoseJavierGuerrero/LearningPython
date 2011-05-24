f79c90c4be5ec5d75518ec510824fdb36c596a06
alejandrogomez <alejandroogomez@gmail.com>
2011-05-25 00:26:24 +0200

    tabs to spaces

--- helloarguments.py
+++ helloarguments.py
@@ -2,19 +2,19 @@
 # encoding: utf-8
 
 def person(name='Edgar', age=18):
-	""" Prints a string using "name" and "age"
-	
-	
-	Returns None.
-	"""
-	print name + ' is ' + str(age)
+    """ Prints a string using "name" and "age"
+    
+    
+    Returns None.
+    """
+    print name + ' is ' + str(age)
 
 
 if __name__ == '__main__':
-	person()
-	person('Miles')
-	person(age=21)
-	person('Damian', 23)
-	person(age=30, name='Herman')
-	
+    person()
+    person('Miles')
+    person(age=21)
+    person('Damian', 23)
+    person(age=30, name='Herman')
+    
 
