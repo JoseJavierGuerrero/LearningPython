51f204bbff374d41ae00061cff258905ebae7b04
Jose J Guerrero Montero <jjguerreromontero@gmail.com>
2011-05-26 15:45:32 +0200

    improvements

--- class_histogram.py
+++ class_histogram.py
@@ -10,7 +10,7 @@ class Histogram(collections.Counter):
     
     def __init__(self, filepath_=None, list_=None):
         """
-		Takes filepath and returns an histogram instance
+        Takes filepath and returns an histogram instance
         wich represents the frequency of the words in the file at 
         filepath or a list of pairs (word, freq)
         
@@ -18,11 +18,11 @@ class Histogram(collections.Counter):
         
         If no dict is given neither filepath (or it doesn't exists) it raises 
         an exception
-		"""
+        """
         
         if filepath_ is not None:
             f = open(filepath_).read()
-            tokens = f.split()
+            tokens = [elem for elem in f.split() if elem.isalnum()]
             self.longest_word = reduce(lambda x, y: max(x,len(y)), tokens, 0)
             collections.Counter.__init__(self, tokens)
         elif list_ is not None:
@@ -36,26 +36,42 @@ class Histogram(collections.Counter):
         
     def __str__(self):
         return '\n'.join([k.ljust(self.longest_word+5) + ": " + '*'*v 
-							for (k, v) in self.items() ])
+                            for (k, v) in self.items() ])
+
+   
+    def filter(self,filter_):
+        """filter an histogram with filter_
+        
+        Returns a new instance with the elements wich key accomplish filter_"""
+        l = [(k, v) for k, v in self.items() if filter_(k)]
+        
+        return Histogram(list_=l)
+        
+    def longer_than(self,n):
+        """Returns a new instance with the words longer than n"""
+        return self.filter(lambda x: len(x) > n)
 
     def top(self,n):
         """Returns a new instance with the top n words ordered by frequency"""
         topn = self.most_common(n)
-        return Histogram(list_=topn)
-   
-    def longer_than(self,n):
-        """Returns a new instance with the words longer than n"""
-        l = [(k, v) for k, v in self.items() if len(k)>n]
-        return Histogram(list_=l)
+        return Histogram(list_=topn)    
+        
+        
+        
+    
 
 if __name__=="__main__":
     try:
-        filepath = "./gpl-3.0.dos.ascii"
+        filepath = "./bible.txt"
         book = Histogram(filepath_=filepath)
-        print book
+        #print book
         print "\nThe ten most common words\n"
-        print book.top(10)
+        #print book.top(10)
         print "\nWords longer than 10"
-        print book.longer_than(10)
+        #print book.longer_than(10)
     except IOError: 
         print "Error opening the file %s" % filepath
+
+
+#TODO number of words, more filters, compare between two files: 
+#longest word, most frequent word, number of words
--- word_counter.py
+++ word_counter.py
@@ -6,6 +6,6 @@ import collections
 
 if __name__=='__main__':
     
-    #print "\n".join([k.ljust(80)+": "+'*'*v for k, v in collections.Counter(open("./gpl-3.0.dos.ascii").read().split()).items()])
+    #print "\n".join([k.ljust(80)+": "+'*'*v for k, v in collections.Counter(open("./gpl-3.0.dos.ascii").read().split()).items() if k.isalnum()])
     print "\n".join([k.ljust(reduce(lambda x, y: max(x,len(y)),open("./gpl-3.0.dos.ascii").read().split(), 0)+2)+": "+'*'*v for k, v \
-                                                            in collections.Counter(open("./gpl-3.0.dos.ascii").read().split()).items()])
+                                                            in collections.Counter(open("./gpl-3.0.dos.ascii").read().split()).items() if k.isalnum()])
