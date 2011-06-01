cf6e5b7fbf1081d8eea73c1d4086ab7a7181513b
Alejandro Gómez <alejandroogomez@gmail.com>
2011-06-02 01:04:09 +0200

    code snippets to pygmentize

---
+++ pygmentize_this.py
@@ -0,0 +1,25 @@
+dic = { 'a': 1, 'b': "dos", 3: 'c', 'd': {'e': 5}}
+
+li = ['a', 2, ["tres", 4], 5]
+
+tupla = ("a", 2, "b", (4, 5))
+
+cadena = "¡Hola mundo!"
+
+print "%s = %d" % cadena entero
+
+urllib.urlopen("http://bit.ly/PythonAP")
+
+import module
+from module import atribute
+
+class Foo: 
+	pass
+	
+class Foo(AnotherClass):
+		
+getmembers(object, predicate)
+
+getsource(object)
+
+getargspec(function)
